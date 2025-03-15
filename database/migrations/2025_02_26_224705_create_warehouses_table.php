226a0a76c9bffae665e9300102808d62f91d6b01
Syed <syed@syed.lan>
2025-03-15 00:46:46 -0700

    added default wherehouse and shelves

--- database/migrations/2025_02_26_224705_create_warehouses_table.php
+++ database/migrations/2025_02_26_224705_create_warehouses_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateWarehousesTable extends Migration
 {
@@ -19,6 +20,18 @@ class CreateWarehousesTable extends Migration
             $table->text('contact_details')->nullable();
             $table->timestamps();
         });
+
+        DB::table('warehouses')->insert([
+            [
+                'name' => 'Default Warehouse',
+                'street' => '123 Main St',
+                'city' => 'Anytown',
+                'state' => 'CA',
+                'zip' => '12345',
+                'country' => 'US',
+                'contact_details' => 'Phone: 123-456-7890',
+            ],
+        ]);
     }
 
     public function down()
--- database/migrations/2025_02_26_224706_create_shelves_table.php
+++ database/migrations/2025_02_26_224706_create_shelves_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateShelvesTable extends Migration
 {
@@ -16,6 +17,12 @@ class CreateShelvesTable extends Migration
 
             $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade');
         });
+
+        DB::table('shelves')->insert([
+            ['warehouse_id' => 1, 'shelf_name' => 'Default-Shelf-AAA'],
+            ['warehouse_id' => 1, 'shelf_name' => 'Default-Shelf-BBB'],
+            ['warehouse_id' => 1, 'shelf_name' => 'Default-Shelf-CCC'],
+        ]);
     }
 
     public function down()
